99aa7dc81f2c8ebefaa35807a7ee2411dca12e75
Muhammadqodir_57 <mirzamatovanasiba@gmail.com>
2024-02-09 19:57:13 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ const bottomLeftValue = document.getElementById('bl-value');
 const bottomRightValue = document.getElementById('br-value');
 
 function updateBorderRadius() {
-    const topLeft = tl.value + 'px';
-    const topRight = tr.value + 'px';
-    const bottomLeft = bl.value + 'px';
-    const bottomRight = br.value + 'px';
+    const topLeft = topLeft.value + 'px';
+    const topRight = topRight.value + 'px';
+    const bottomLeft = bottomLeft.value + 'px';
+    const bottomRight = bottomRight.value + 'px';
     const borderRadius = `${topLeft} ${topRight} ${bottomRight} ${bottomLeft}`;
     elBox.style.borderRadius = borderRadius;
 

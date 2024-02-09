7bde5486f12c6092a80b3d1684e51cb1e6af4070
Muhammadqodir_57 <mirzamatovanasiba@gmail.com>
2024-02-09 20:27:39 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-const elBox = document.getElementById('box');
-const topLeft = document.getElementById('tl');
-const topRight = document.getElementById('tr');
-const bottomLeft = document.getElementById('bl');
-const bottomRight = document.getElementById('br');
+const box = document.getElementById('box');
+const tl = document.getElementById('tl');
+const tr = document.getElementById('tr');
+const bl = document.getElementById('bl');
+const br = document.getElementById('br');
 
-const topLeftValue = document.getElementById('tl-value');
-const topRightValue = document.getElementById('tr-value');
-const bottomLeftValue = document.getElementById('bl-value');
-const bottomRightValue = document.getElementById('br-value');
+const tlValue = document.getElementById('tl-value');
+const trValue = document.getElementById('tr-value');
+const blValue = document.getElementById('bl-value');
+const brValue = document.getElementById('br-value');
 
 function updateBorderRadius() {
-    const topLeft = topLeft.value + 'px';
-    const topRight = topRight.value + 'px';
-    const bottomLeft = bottomLeft.value + 'px';
-    const bottomRight = bottomRight.value + 'px';
+    const topLeft = tl.value + 'px';
+    const topRight = tr.value + 'px';
+    const bottomLeft = bl.value + 'px';
+    const bottomRight = br.value + 'px';
     const borderRadius = `${topLeft} ${topRight} ${bottomRight} ${bottomLeft}`;
-    elBox.style.borderRadius = borderRadius;
+    box.style.borderRadius = borderRadius;
 
-    topLeftValue.value = topLeft.value;
-    topRightValue.value = topRight.value;
-    bottomLeft.value = bottomLeft.value;
-    bottomRight.value = borderRadius.value;
+    tlValue.value = tl.value;
+    trValue.value = tr.value;
+    blValue.value = bl.value;
+    brValue.value = br.value;
 }
 
-topLeft.addEventListener('input', updateBorderRadius);
-topRight.addEventListener('input', updateBorderRadius);
-bottomLeft.addEventListener('input', updateBorderRadius);
-bottomRight.addEventListener('input', updateBorderRadius);
+tl.addEventListener('input', updateBorderRadius);
+tr.addEventListener('input', updateBorderRadius);
+bl.addEventListener('input', updateBorderRadius);
+br.addEventListener('input', updateBorderRadius);
